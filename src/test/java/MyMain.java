b8134c2f06bfe92437cc62dde1927359601896a1
xueyi123 <qq101388>
2016-02-11 21:12:58 +0800

    iih5

--- src/test/java/MyMain.java
+++ src/test/java/MyMain.java
@@ -1,10 +1,12 @@
+import com.iih5.smartorm.generator.ModelGenerator;
+
 public class MyMain {
 
     public static void main(String[] args) throws Exception {
         System.out.println("hello world");
         SpringContext.getInstace();
 
-       // ModelGenerator.generator("parkdb","com.tthd.model.generator","D:/ideaProject/smartorm/src/main/java");
+        ModelGenerator.generator("parkdb","com.tthd.model.generator","D:/ideaProject/smartorm/src/main/java");
     }
 
 }
