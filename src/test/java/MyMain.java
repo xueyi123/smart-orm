ec59846311d19432b6198ec9bb36e6c4e035c3a8
xueyi123 <qq101388>
2016-02-14 15:37:26 +0800

    iih5

--- src/test/java/MyMain.java
+++ src/test/java/MyMain.java
@@ -9,21 +9,6 @@ public class MyMain {
         SpringContext.getInstace();
 
       //  ModelGenerator.generator("parkdb","com.tthd.model.generator","D:/ideaProject/smartorm/src/main/java");
-        Redis.subscribe(new JedisPubSub() {
-            @Override
-            public void onMessage(String channel, String message) {
-                System.out.println("msg="+message);
-            }
-        },"test");
-        for (int i = 0; i <1000 ; i++) {
-            Redis.set("dd","dd");
-        }
-        Redis.publish("test","hahahah....");
-        System.out.println( Redis.getNumIdlePool());
-        System.out.println( Redis.getNumActivePool());
-        System.out.println(Redis.getNumWaitersPool());
-
-
     }
 
 }
