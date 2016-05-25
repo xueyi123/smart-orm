5f1404ae28a9c5775755ceb172e2f32e8f3d12ef
xueyi123 <qq101388>
2016-05-25 22:24:58 +0800

    iih5

--- src/test/java/MyMain.java
+++ src/test/java/MyMain.java
@@ -1,11 +1,13 @@
+import com.iih5.smartorm.generator.ModelGenerator;
 public class MyMain {
 
     public static void main(String[] args) throws Exception {
         System.out.println("hello world");
         SpringContext.getInstace();
-       // System.out.println("开始。。。");
-       // ModelGenerator.generator("parkdb","com.tthd.model.generator","D:/ideaProject/smart-orm/src/main/java");
-      //  System.out.println("。。。结束");
+        System.out.println("开始。。。");
+        ModelGenerator.generator("parkdb","com.tthd.model.generator","D:/ideaProject/smart-orm/src/main/java");
+        System.out.println("。。。结束");
+
 
     }
 
